afe980daf5c60756f805efb585218dcad8c8c658
AmineAffif <affif.amine@live.fr>
2023-11-11 13:53:11 +0100

    feat: add models for Products, Brands, Categories, Orders, and OrderItems

---
+++ db/migrate/20231111122739_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price, precision: 10, scale: 2
+      t.integer :inventory
+      t.references :category, null: false, foreign_key: true
+      t.references :brand, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
