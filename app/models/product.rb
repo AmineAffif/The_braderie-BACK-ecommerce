d06c26a6fcc81986e20f72379e6ad1c0c9d3dfd4
AmineAffif <affif.amine@live.fr>
2023-11-12 13:39:29 +0100

    refacto: refacto for performences and some validation for product creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items
 
+  validates :name, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+
   def formatted_price
     sprintf('%.2f', price)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'orders/create'
-  get 'products/index'
-  get 'products/show'
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # Routes pour les produits
   resources :products, only: [:index, :show]
-
-  # Route pour créer une commande
-  post '/orders', to: 'orders#create'
-  
+  resources :orders, only: [:create]
 end
