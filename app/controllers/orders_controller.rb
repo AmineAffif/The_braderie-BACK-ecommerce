3bcbbae9dae272d91da368a76e5dc70eb383f337
AmineAffif <affif.amine@live.fr>
2023-11-12 20:43:07 +0100

    feat: Stock control

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,23 @@
 class OrdersController < ApplicationController
   def create
-    order = Order.new(order_params)
-    if order.save
-      render json: order, status: :created
-    else
-      render json: order.errors, status: :unprocessable_entity
+    ActiveRecord::Base.transaction do
+      order = Order.new(order_params)
+  
+      order.order_items.each do |item|
+        product = Product.find(item.product_id)
+        raise ActiveRecord::Rollback if item.quantity > product.inventory
+      end
+  
+      if order.save
+        order.order_items.each do |item|
+          product = Product.find(item.product_id)
+          product.update!(inventory: product.inventory - item.quantity)
+        end
+  
+        render json: order, status: :created
+      else
+        render json: order.errors, status: :unprocessable_entity
+      end
     end
   end
 
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,18 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
-  
+
   validates :quantity, :price, presence: true
+  validate :product_quantity_available
+
+  private
+
+  def product_quantity_available
+    return if product.nil? || quantity.nil?
+
+    if quantity > product.inventory
+      errors.add(:quantity, 'exceeds available stock')
+    end
+  end
+
 end
